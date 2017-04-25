b51ee8d3f3317605d90790d870cfbb4cb1a52c3a
bsellars <bsellars@squiz.co.nz>
2017-04-25 21:17:58 +1200

    Transpiling through gulp now

--- public/assets/components.js
+++ public/assets/components.js
@@ -266,6 +266,10 @@ class CommentRemoveConfirmation extends React.Component {
 jQuery(function() {
   ReactDOM.render(
     <CommentBox />,
-    document.getElementById('comment-box')
+    document.getElementById('comment-box'),
+    function() {
+      console.timeEnd('react-app')
+    }
+
   );
 })
