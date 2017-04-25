b95306beab72b5ac62ccc5e7ab3323ee6e938385
bsellars <bsellars@squiz.co.nz>
2017-04-25 21:56:02 +1200

    Separated each class into its own  odule

---
+++ src/components/comment.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import CommentConfirmation from './comment-confirmation';
+
+export default class Comment extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isAbusive: false
+    };
+  }
+
+  render() {
+
+    let commentBody;
+
+    if (!this.state.isAbusive) {
+      commentBody = this.props.body;
+    } else {
+      commentBody = <em>Content marked as abusive</em>;
+    }
+
+    return(
+      <div className="comment">
+
+        <img src={this.props.avatarUrl} alt={`${this.props.author}'s picture`} />
+
+        <p className="comment-header">{this.props.author}</p>
+        <p className="comment-body">{commentBody}</p>
+
+        <div className="comment-actions">
+          <CommentConfirmation onConfirm={this._handleDelete.bind(this)}>
+            Delete Comment?
+          </CommentConfirmation>
+          <a href="#" onClick={this._toggleAbuse.bind(this)}>Report as Abuse</a>
+        </div>
+      </div>
+    );
+  }
+
+  _toggleAbuse(event) {
+    event.preventDefault();
+
+    this.setState({
+      isAbusive: !this.state.isAbusive
+    });
+  }
+
+  _handleDelete() {
+    this.props.onDelete(this.props.id);
+  }
+}
